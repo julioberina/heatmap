7c4d0dc5e99363b09374433f0de5be01b8577d90
Julio Berina <julioberina@gmail.com>
2019-05-03 22:30:10 -0700

    Add blank heatmap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: [],
+      x: 0,
+      y: 0
+    };
+
+    this.heat = this.heat.bind(this);
+    this.reset = this.reset.bind(this);
+
+    for (let i = 0; i < 52; ++i) {
+      this.state.data.push({"days": [0, 0, 0, 0, 0, 0, 0]})
+    }
   }
 
   heat(e) {
     e.preventDefault();
     console.log('clicked submit');
+  }
+
+  reset(e) {
+    e.preventDefault();
+    console.log('clicked reset');
+    let newdata = [];
+
+    for (let i = 0; i < 52; ++i) {
+      newdata.push({"days": [0, 0, 0, 0, 0, 0, 0]})
+    }
+
+    this.setState({ data: newdata });
   }
 
   render() {
@@ -28,6 +52,41 @@ class App extends Component {
 
         <div align="center">
           <button type="submit" className="submitInfo" onClick={this.heat}>Submit</button>
+          <button className="resetInfo" onClick={this.reset}>Reset</button>
+        </div>
+
+        <br />
+
+        <div align="center" className="heatmap">
+          <h3>2018 Heatmap</h3>
+          <table>
+            <tbody>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     );
