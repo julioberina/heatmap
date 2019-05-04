ee53eb4d6e00e269ae0f69f3d68109e99626ce89
Julio Berina <julioberina@gmail.com>
2019-05-04 00:33:13 -0700

    Updates

---
+++ precache-manifest.d3c8da68c1cfc487941c94fce85b4dc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f64f70558708e0eb7dc7397469b906",
+    "url": "/heatmap/index.html"
+  },
+  {
+    "revision": "675687e597db0bf7ad61",
+    "url": "/heatmap/static/css/main.64fc5fe5.chunk.css"
+  },
+  {
+    "revision": "9e7787341f15e4ebb3e5",
+    "url": "/heatmap/static/js/2.4b946ab3.chunk.js"
+  },
+  {
+    "revision": "675687e597db0bf7ad61",
+    "url": "/heatmap/static/js/main.982634bb.chunk.js"
+  },
+  {
+    "revision": "e6b2793c0c8da9575b6e",
+    "url": "/heatmap/static/js/runtime~main.6924498e.js"
+  }
+]);
