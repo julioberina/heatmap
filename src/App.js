302c37d1aab5d1649a6e0f70d69f2b47103c1f8d
Julio Berina <julioberina@gmail.com>
2019-05-03 20:51:28 -0700

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  heat(e) {
+    e.preventDefault();
+    console.log('clicked submit');
+  }
+
+  render() {
+    return (
+      <div>
+        <div align="center" className="top">
+          <h1>Github Repo Heatmap</h1>
+        </div>
+
+        <div align="center" className="container">
+          <input id="username" type="text" placeholder="Enter Github Username" />
+          <input id="reponame" type="text" placeholder="Enter Github Repository Name" />
+        </div>
+
+        <br />
+
+        <div align="center">
+          <button type="submit" className="submitInfo" onClick={this.heat}>Submit</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
